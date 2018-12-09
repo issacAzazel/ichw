8043ddd5f75d07ec224a95e6e1095fdce08fa0e3
lxh <lxh@lxhdeMacBook-Pro.local>
2018-12-09 23:11:50 +0800

    add some features

--- pyassign3/tile.py
+++ pyassign3/tile.py
@@ -6,7 +6,6 @@ __email__  = "linxiaohan@pku.edu.cn"
 '''
 
 
-
 import turtle
 
 
@@ -61,32 +60,43 @@ def isin(list_1,list_0):
 
 
 
+def maxmincoordinate(list_1):
+    '找到一系列坐标里最大的一个和最小的一个'
+    max_coordinate=list_1[0]
+    min_coordinate=list_1[0]
+    for j in range(len(list_1)):
+        if list_1[j][0]+list_1[j][1]>max_coordinate[0]+max_coordinate[1]:
+            max_coordinate=list_1[j]
+        if list_1[j][0]+list_1[j][1]<min_coordinate[0]+min_coordinate[1]:
+            min_coordinate=list_1[j]
+    return [max_coordinate,min_coordinate]
+
+
+
 def canfill(coordinate,m,n,a,b):
-    '判断在一系列坐标下a*b的长方形能不能填充，并且返回方案'
+    '判断在一系列坐标下a*b的长方形能不能被填充，并且返回方案'
     boole_1=False
     boole_2=False
     parts=[]
+    min_coordinate=maxmincoordinate(coordinate)[1]
     if a!=b:
-        for i in coordinate:
-            list_1=seriescoordinate(i,a,b)[0]
-            list_2=seriescoordinate(i,a,b)[1]
-            if isin(list_1,coordinate):
-                parts.append(number(list_1,m,n))
-                boole_1=True
-            if isin(list_2,coordinate):
-                parts.append(number(list_2,m,n))
-                boole_1=True
+        list_1=seriescoordinate(min_coordinate,a,b)[0]
+        list_2=seriescoordinate(min_coordinate,a,b)[1]
+        if isin(list_1,coordinate):
+            parts.append(number(list_1,m,n))
+            boole_1=True
+        if isin(list_2,coordinate):
+            parts.append(number(list_2,m,n))
+            boole_1=True
         boole=boole_1 or boole_2
     else:
-        for i in coordinate:
-            list_1=seriescoordinate(i,a,b)[0]
-            list_2=seriescoordinate(i,a,b)[1]
-            if isin(list_1,coordinate):
-                parts.append(number(list_1,m,n))
-                boole_1=True
+        list_1=seriescoordinate(min_coordinate,a,b)[0]
+        list_2=seriescoordinate(min_coordinate,a,b)[1]
+        if isin(list_1,coordinate):
+            parts.append(number(list_1,m,n))
+            boole_1=True
         boole=boole_1
     return[boole,parts]
-            
 
 
 
@@ -137,34 +147,6 @@ def nonegetaway(solutions):
 
 
 
-def issame(solution_1,solution_2):
-    '判断两个方案是不是一样的'
-    for i in solution_1:
-        if i not in solution_2:
-            return False
-    return True
-
-
-
-def isinsolution(solution_1,solution):
-    '判断这个方案是不是已经在某个方案列表中'
-    for i in solution:
-        if issame(i,solution_1):
-            return True
-    return False
-
-
-
-def repeatergetaway(solutions):
-    '去掉方案中的重复项'
-    solutions_nonrepeat=[]
-    for i in solutions:
-        if not isinsolution(i,solutions_nonrepeat):
-            solutions_nonrepeat.append(i)
-    return (solutions_nonrepeat)
-
-
-
 def tuplize(solutions):
     '把方案改写成tuple的形式'
     solution_tuple=[]
@@ -174,24 +156,14 @@ def tuplize(solutions):
             each_solution_tuple.append(tuple(j))
         solution_tuple.append(each_solution_tuple)
     return solution_tuple
-  
-
-
-def maxmincoordinate(list_1):
-    '找到一系列坐标里最大的一个和最小的一个'
-    max_coordinate=list_1[0]
-    min_coordinate=list_1[0]
-    for j in range(len(list_1)):
-        if list_1[j][0]+list_1[j][1]>max_coordinate[0]+max_coordinate[1]:
-            max_coordinate=list_1[j]
-        if list_1[j][0]+list_1[j][1]<min_coordinate[0]+min_coordinate[1]:
-            min_coordinate=list_1[j]
-    return [max_coordinate,min_coordinate]
 
 
 
-def drawasquare(bob,left_corner,right_corner):
+
+def drawasquare(bob,left_corner,right_corner,color,size):
     '在知道左上角和右下角坐标的情况下画一个长方形'
+    bob.color(color)
+    bob.pensize(size)
     bob.penup()
     bob.goto(left_corner[0],left_corner[1])
     bob.pendown()
@@ -206,7 +178,7 @@ def drawasquare(bob,left_corner,right_corner):
 
 
 
-def turtles_see_you_again(solution,m,n,a,b):
+def turtles_see_you_again(solution,m,n,a,b,color,size):
     '小乌龟我们又见面了TAT'
     bg=turtle.Screen()
     bg.screensize(1500, 1500)
@@ -218,7 +190,7 @@ def turtles_see_you_again(solution,m,n,a,b):
         min_coordinate=maxmincoordinate(list_1)[1]
         left_corner=[(min_coordinate[0])*50,-(min_coordinate[1])*50]
         right_corner=[(max_coordinate[0]+1)*50,-(max_coordinate[1]+1)*50]
-        drawasquare(bob,left_corner,right_corner)
+        drawasquare(bob,left_corner,right_corner,color,size)
 
 
 
@@ -237,15 +209,18 @@ def main():
             print('不能铺满')
         else:
             list_answer_1=nonegetaway(list_answer)
-            list_answer_2=repeatergetaway(list_answer_1)
-            list_answer_3=tuplize(list_answer_2)
+            list_answer_2=tuplize(list_answer_1)
             number=1
-            for solution in list_answer_3:
+            for solution in list_answer_2:
                 print(str(number)+': ',end='')
                 print(solution)
                 number+=1
             t=int(input('请选择一种进行可视化: '))
-            turtles_see_you_again(list_answer_2[t-1],m,n,a,b)
+            turtles_see_you_again(list_answer_1[t-1],m,n,a,b,'blue',5)
+            list_all=[]
+            for i in range(m*n):
+                list_all.append([i])
+            turtles_see_you_again(list_all,m,n,a,b,'black',1)
 
     
 
